3c68c7758cfe72d2fd41c77b99065cd72e7b2079
MariaBut <112708831+MariaBut@users.noreply.github.com>
2022-10-05 00:56:38 +0300

    Initial commit

---
+++ date_fact_api.js
@@ -0,0 +1,16 @@
+const get_api_res_btn = document.getElementById('get_api_res_btn')
+const api_result = document.getElementById('api_result')
+const uidate = document.getElementById('uidate')
+const uimonth = document.getElementById('uimonth');
+
+get_api_res_btn.addEventListener('click', getAPI);
+    function getAPI(){
+        let endPoint = "http://numbersapi.com/" + uimonth.value + "/" + uidate.value + "/date";
+        fetch(endPoint, {
+		method: 'GET'
+	})
+    .then(res => res.text())
+    .then(data=> {
+        api_result.innerHTML = data;
+    });
+};
